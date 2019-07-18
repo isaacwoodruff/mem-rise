99501682ba07b09649096f9684de933d75f877ae
isaacwoodruff <isaacjahwoodruff@gmail.com>
2019-07-18 13:20:01 +0000

    Added score text and updated when and where it is displayed

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,24 +3,24 @@
 // When Start button is clicked it hides the main menu, then shows the game view and back/menu icons
 $('.start-btn').click(function() {
     $('.main-menu').addClass('d-none');
-    $('.game-view, .back-and-menu-icons').removeClass('d-none');
+    $('.game-view, .back-and-menu-icons, .score').removeClass('d-none');
 });
 
 // When How To Play/ Help link is clicked it hides the main menu, menu list, then game view and shows the help menu
 $('.help-menu-link').click(function() {
-    $('.main-menu, .menu-list, .game-view').addClass('d-none');
+    $('.main-menu, .menu-list, .game-view, .score').addClass('d-none');
     $('.help-menu, .back-and-menu-icons').removeClass('d-none');
 });
 
 // When the Main Menu link is clicked in the menu list it hides the menu list and back/menu icons, then shows the main menu
 $('.main-menu-link').click(function() {
-    $('.menu-list, .back-and-menu-icons').addClass('d-none');
+    $('.menu-list, .back-and-menu-icons, .score').addClass('d-none');
     $('.main-menu').removeClass('d-none');
 });
 
 // When the 3 stacked bars icon is clicked it hides the help menu and game view, then shows the menu list
 $('.fa-bars').click(function() {
-    $('.help-menu, .game-view').addClass('d-none');
+    $('.help-menu, .game-view, .score').addClass('d-none');
     $('.back-and-menu-icons, .menu-list').removeClass('d-none');
 });
 
@@ -31,12 +31,12 @@ $('.fa-bars').click(function() {
 */
 $('.fa-arrow-left').click(function() {
     if (!$('.game-view').hasClass('d-none')) {
-        $('.game-view, .back-and-menu-icons').addClass('d-none');
+        $('.game-view, .back-and-menu-icons, .score').addClass('d-none');
         $('.main-menu').removeClass('d-none');
     }
     else {
         $('.main-menu, .menu-list, .help-menu').addClass('d-none');
-        $('.game-view, .back-and-menu-icons').removeClass('d-none');
+        $('.game-view, .back-and-menu-icons, .score').removeClass('d-none');
     }
 });
 
